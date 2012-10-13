119bfd768332705436c8cfbaba7b0beae3c1ce9d
batterytime <stephenjchen@gmail.com>
2012-10-13 13:12:52 -0700

    smoothly moving buses

--- static/js/maps.js
+++ static/js/maps.js
@@ -30,7 +30,40 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
   var jMapDiv = $("#mapCanvas");
   var map;
   var openInfoWindow;
+  var busWithWindow;
   var busMarkers = new Array();
+  
+  function HomeControl(controlDiv, map) {
+
+  // Set CSS styles for the DIV containing the control
+  // Setting padding to 5 px will offset the control
+  // from the edge of the map.
+  controlDiv.style.padding = '5px';
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = 'white';
+  controlUI.style.borderStyle = 'solid';
+  controlUI.style.borderWidth = '2px';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to set the map to Home';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.fontFamily = 'Arial,sans-serif';
+  controlText.style.fontSize = '12px';
+  controlText.style.paddingLeft = '4px';
+  controlText.style.paddingRight = '4px';
+  controlText.innerHTML = '<strong>Home</strong>';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  google.maps.event.addDomListener(controlUI, 'click', function() {
+    alert("test");
+  });
+}
   
   var tid = setInterval(intervalLoop, 10000);
   function intervalLoop() {
@@ -40,13 +73,24 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
     clearInterval(tid);
   }
   
-  var glid = setInterval(graphicalLoop, 50);
+  var glid = setInterval(graphicalLoop, 20);
   function graphicalLoop() {
     for(index in busMarkers) {
       var bus = busMarkers[index];
       var elapsed = Date.now()-bus.reportTime;
-      var extrapolatedLatLng = bus.originalLatLng.destinationPoint(bus.bearing, bus.speed*(bus.timeSinceRefresh/3600+elapsed/3600000));
-      bus.setPosition(extrapolatedLatLng);
+      var SMOOTHMOVETIME = 1000.0;
+      if(elapsed < SMOOTHMOVETIME && bus.timeSinceRefresh <= 20) {
+        var oneSecExtrapolatedLatLng = bus.originalLatLng.destinationPoint(bus.bearing, bus.speed*((elapsed/SMOOTHMOVETIME+bus.timeSinceRefresh)/3600));
+        var latDiff = oneSecExtrapolatedLatLng.lat() - bus.animStartLatLng.lat();
+        var lngDiff = oneSecExtrapolatedLatLng.lng() - bus.animStartLatLng.lng();
+        var factor = Math.sqrt(elapsed/SMOOTHMOVETIME);
+        latDiff *= factor;
+        lngDiff *= factor;
+        bus.setPosition(new google.maps.LatLng(bus.animStartLatLng.lat()+latDiff, bus.animStartLatLng.lng()+lngDiff));
+      } else {
+        var extrapolatedLatLng = bus.originalLatLng.destinationPoint(bus.bearing, bus.speed*(bus.timeSinceRefresh/3600+elapsed/3600000));
+        bus.setPosition(extrapolatedLatLng);
+      }
     }
   }
 
@@ -54,6 +98,11 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
     mapSetup(position);
     plotRoutes(position);
     get51B();
+    var homeControlDiv = document.createElement('div');
+    var homeControl = new HomeControl(homeControlDiv, map);
+
+    homeControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
   });
   
   function clearBuses() {
@@ -64,7 +113,7 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
   }
   
   function get51B() {
-    clearBuses();
+    var newBusMarkers = new Array();
     var routeId = '51B';
     $.get("http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=actransit&r="+routeId+"&t=0", function(res){
       var parsed = $.parseXML(res);
@@ -75,31 +124,54 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
         var image = 'bus-icon.gif';
         var myLatLng = new google.maps.LatLng(lat, lon);
         var extrapolatedLatLng = myLatLng.destinationPoint(parseInt($(this).attr("heading")), parseFloat($(this).attr("speedKmHr"))*parseFloat($(this).attr("secsSinceReport"))/3600);
+        
+        var existingBus;
+        for(var i = 0; i < busMarkers.length; i++) {
+            var bus = busMarkers[i];
+            console.log("TRYING TO MATCH: "+parseInt($(this).attr("id")));
+            console.log(parseInt(bus.id));
+            if (parseInt(bus.id) == parseInt($(this).attr("id"))) {
+                extrapolatedLatLng = bus.getPosition();
+                console.log("BUS UPDATED AND KEPT: "+bus.id);
+            }
+        }
+        
         var busMarker = new google.maps.Marker({
           position: extrapolatedLatLng,
           map: map,
           icon: image
         });
+        busMarker.id = $(this).attr("id");
         busMarker.originalLatLng = myLatLng;
+        busMarker.animStartLatLng = busMarker.position;
         busMarker.bearing = parseFloat($(this).attr("heading"));
         busMarker.speed = parseFloat($(this).attr("speedKmHr"));
         busMarker.reportTime = Date.now();
         busMarker.timeSinceRefresh = parseFloat($(this).attr("secsSinceReport"));
-        busMarkers.push(busMarker);
-        var contentString = "<h1 style='font-family:Segoe UI; font-size:2em'>" + $(this).attr("routeTag") + "</h1>";
+        newBusMarkers.push(busMarker);
+        var contentString = "<h1 style='font-family:Segoe UI; font-size:2em'>" + $(this).attr("routeTag") + " - "+busMarker.id+"</h1>";
         contentString += "<p style='font-family:Calibri'>Last updated "+$(this).attr("secsSinceReport")+" seconds ago";
 
         var infowindow = new google.maps.InfoWindow({
           content : contentString
         });
+        
+        if(busWithWindow && busWithWindow.id == busMarker.id) {
+          infowindow.open(map,busMarker);
+          openInfoWindow = infowindow;
+          busWithWindow = busMarker;
+        }
 
         google.maps.event.addListener(busMarker, 'click', function() {
           if(openInfoWindow)
             openInfoWindow.close();
           infowindow.open(map,busMarker);
           openInfoWindow = infowindow;
+          busWithWindow = busMarker;
         });
       });
+      clearBuses();
+      busMarkers = newBusMarkers;
     });
   }
 
@@ -200,6 +272,7 @@ google.maps.LatLng.prototype.destinationPoint = function(brng, dist) {
                 openInfoWindow.close();
               infowindow.open(map,marker);
               openInfoWindow = infowindow;
+              busWithWindow = null;
             });
           }
         })(point);
